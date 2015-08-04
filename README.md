2fd06beeb1338f1d3ec26f92123b9db916596018
Maria <mcarbmont@gmail.com>
2015-08-04 18:56:15 -0500

    Initial Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# JSLinted Express project
+
+This project was successfully corrected using JS Lint. The project structure is the one that the "Express Generator" generates.
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+/*jslint node: true, indent: 2,nomen:true */
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+/*jslint unparam: true */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+/*jslint unparam: false */
+
+module.exports = router;
