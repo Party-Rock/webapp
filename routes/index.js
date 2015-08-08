f0d90716e31cbaa9873c24f72c27d5d3bfd9c20e
Maria <mcarbmont@gmail.com>
2015-08-07 21:41:25 -0500

    :smirk:3 pages

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 /* GET home page. */
 /*jslint unparam: true */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('search_view', { title: 'Express' });
 });
 /*jslint unparam: false */
 
