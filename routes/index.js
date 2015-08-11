a552f9a0e1cf2a6e3f519d1a644bf90cae98c435
Maria <mcarbmont@gmail.com>
2015-08-11 16:02:21 -0500

    static views :grin:

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,19 @@ var router = express.Router();
 /* GET home page. */
 /*jslint unparam: true */
 router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/second', function (req, res, next) {
+  res.render('second_view', { title: 'Express' });
+});
+
+router.get('/search', function (req, res, next) {
   res.render('search_view', { title: 'Express' });
+});
+
+router.get('/confirm', function (req, res, next) {
+  res.render('confirmar_renta', { title: 'Express' });
 });
 /*jslint unparam: false */
 
